d8de1064626a8f5819a07cffe37313b64793c32c
Pedro Fontes <pedrofontes@pop-os.localdomain>
2021-09-22 12:43:25 -0300

    a

--- install.sh
+++ install.sh
@@ -4,6 +4,13 @@ sudo apt upgrade -y
 
 # Install apt programs
 sudo apt remove vim -y
-sudo apt install neovim -y
+sudo apt install neovim tmux code -y
 
-sudo apt install tmux -y
+# LSD
+wget https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd_0.20.1_amd64.deb
+sudo dpkg -i lsd_0.20.1_amd64.deb
+sudo rm lsd_0.20.1_amd64.deb
+
+echo "alias ls='lsd -l --blocks permission,user,group,size,name'" >> ~/.bashrc
+
+## instalar nerdfonts 
